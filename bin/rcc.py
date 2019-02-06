c5a06b127d1680f3b8ffd4ccf3a0510ad5e98d4c
Ignacio Tripodi <ignacio.tripodi@colorado.edu>
2019-02-05 23:20:16 -0700

    Cleanup

--- bin/rcc.py
+++ bin/rcc.py
@@ -8,29 +8,29 @@ def calmp(num_of_reads, total_reads):
 
 
 def gettotalreadsfromflagstat(flagstatfile):
-	f = open(flagstatfile)
+        f = open(flagstatfile)
         lines = f.readlines()
         mapped_reads = lines[0]
 #        mapped_reads = int(x[0])
-	print "mapped_reads", mapped_reads
-	return mapped_reads
+#        print "mapped_reads", mapped_reads
+        return mapped_reads
 
 
 def main(Bedgraphfile, flagstatfile, readcountcorrBedgraphfile):
-	total_reads = gettotalreadsfromflagstat(flagstatfile)
-	wf = open(readcountcorrBedgraphfile, "w")
-	f = open(Bedgraphfile)
-	for line in f:
-		line = line.strip("\n")
-		line = line.split("\t")
-		num_of_reads = line[-1]
-		mp = calmp(num_of_reads, total_reads)
-		line[-1] = mp
-		wf.write("\t".join(map(str,line))+"\n")
-	wf.close()
-	f.close()
+        total_reads = gettotalreadsfromflagstat(flagstatfile)
+        wf = open(readcountcorrBedgraphfile, "w")
+        f = open(Bedgraphfile)
+        for line in f:
+                line = line.strip("\n")
+                line = line.split("\t")
+                num_of_reads = line[-1]
+                mp = calmp(num_of_reads, total_reads)
+                line[-1] = mp
+                wf.write("\t".join(map(str,line))+"\n")
+        wf.close()
+        f.close()
 
 
 if __name__=="__main__":
-	Bedgraphfile, flagstatfile, readcountcorrBedgraphfile = sys.argv[1],sys.argv[2], sys.argv[3]
-	main(Bedgraphfile, flagstatfile, readcountcorrBedgraphfile)
+        Bedgraphfile, flagstatfile, readcountcorrBedgraphfile = sys.argv[1],sys.argv[2], sys.argv[3]
+        main(Bedgraphfile, flagstatfile, readcountcorrBedgraphfile)
