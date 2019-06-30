92f9f6d1ce5b762a126aa3c665cd1c88c1801414
Edmund Miller <edmund.a.miller@gmail.com>
2019-06-29 21:54:55 -0500

    feat(edgeR): a new beginning

--- scripts/dge.R
+++ scripts/dge.R
@@ -1,23 +1,30 @@
-library(tidyr)
+## FIXME BiocManager::install("edgeR")
+
+library(readr)
 library(edgeR)
 library(limma)
 library(org.Hs.eg.db)
 
 ## Read in Genes
-raw <- read_tsv(snakemake@input[[1]])
+raw <- read.delim(snakemake@input[[1]],stringsAsFactors=FALSE,check.names=FALSE,)
 
 ## Take column 7 and beyond for counts
 ## First Column is the RefSeqID
 y <- DGEList(counts=raw[,7:ncol(raw)], genes=raw[,1])
-head(y)
+head(y$genes)
 
 ## Keep only transcripts with IDs in NCBI
-idfound <- y$genes$RefSeqID %in% mappedRkeys(org.Hs.egREFSEQ)
-y <- y[idfound,]
+## idfound <- y$genes %in% mappedRkeys(org.Hs.egREFSEQ)
+## y <- y[idfound,]
+## head(y$genes)
 
+print(0)
 egREFSEQ <- toTable(org.Hs.egREFSEQ)
-m <- match(y$genes$RefSeqID, egREFSEQ$accession)
+print(2)
+m <- match(y$genes, egREFSEQ$accession)
+print(3)
 y$genes$EntrezGene <- egREFSEQ$gene_id[m]
+print(4)
 egSYMBOL <- toTable(org.Hs.egSYMBOL)
 
 m <- match(y$genes$EntrezGene, egSYMBOL$gene_id)
@@ -25,8 +32,11 @@ y$genes$Symbol <- egSYMBOL$symbol[m]
 head(y$genes)
 
 ## Remove low counts
+print(5)
 keep <- filterByExpr(y, design)
+print(6)
 y <- y[keep,,keep.lib.sizes=FALSE]
+print(7)
 dge <- calcNormFactors(dge)
 
 ## limma-trend
