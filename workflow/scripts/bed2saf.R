c6258b37261f3445806a1dfb2f37c1b44dbb5349
Edmund Miller <edmund.a.miller@protonmail.com>
2021-01-21 18:55:57 -0600

    chore: Clean out old Snakemake parts of the workflow
    TASK-4

--- workflow/scripts/bed2saf.R
+++
@@ -1,4 +0,0 @@
-saf <- read.table(snakemake@input[[1]])
-colnames(saf) <- c("Chr", "Start", "End", "GeneID", "frame", "Strand")
-saf <- saf[, c("GeneID", "Chr", "Start", "End", "Strand")]
-write.table(saf, snakemake@output[[1]], sep = "\t", col.names = TRUE, row.names = FALSE)
--- workflow/scripts/bed2saf.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-import pandas as pd
-
-bed = pd.read_csv(snakemake.input[0], sep='\t')
-bed.columns = ["Chr", "Start", "End", "GeneID", "frame", "Strand"]
-bed.to_csv(snakemake.output[0], sep='\t',columns=["GeneID", "Chr", "Start", "End", "Strand"],index=False)
