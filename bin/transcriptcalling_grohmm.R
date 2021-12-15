e93e41096265a047ea272ce0ce1ab68d96284918
Edmund Miller <edmund.a.miller@protonmail.com>
2021-12-15 11:56:13 -0600

    refactor(grohmm): Move transcriptcalling to argparse
    Co-authored-by: sruthipsuresh <58604926+sruthipsuresh@users.noreply.github.com>

--- bin/transcriptcalling_grohmm.R
+++ bin/transcriptcalling_grohmm.R
@@ -5,100 +5,107 @@
 # Packages below need to be available to load when running R.
 
 
-suppressPackageStartupMessages(library(optparse))
 suppressPackageStartupMessages(library(argparse))
 suppressPackageStartupMessages(library(GenomicFeatures))
 suppressPackageStartupMessages(library(GenomicAlignments))
 suppressPackageStartupMessages(library(groHMM))
 
-option_list <- list(
-    make_option(c("-i", "--bam_file"),
-        type = "character",
-        default = NULL,
-        metavar = "path",
-        help = "Time course of GRO SEQ data in bam files."
-    ),
-    make_option(c("-t", "--tuning_file"),
-        type = "character",
-        default = NULL,
-        metavar = "path",
-        help = "File with tuning parameters and error rates."
-    ),
-    make_option(c("-o", "--outdir"),
-        type = "character",
-        default = "./",
-        metavar = "path",
-        help = "Output directory."
-    ),
-    make_option(c("-l", "--ltprobb"),
-        type = "integer",
-        default = -200,
-        metavar = "integer",
-        help = "Log-transformed transition probability of switching from transcribed state to non-transcribed state"
-    ),
-    make_option(c("-u", "--uts"),
-        type = "integer",
-        default = 5,
-        metavar = "integer",
-        help = "Variance of the emission probability for reads in the non-transcribed state, respectively."
-    ),
-    make_option(c("-p", "--outprefix"),
-        type = "character",
-        default = "grohmm",
-        metavar = "string",
-        help = "Output prefix."
-    ),
-    make_option(c("-g", "--gtf"),
-        type = "character",
-        default = NULL,
-        metavar = "string",
-        help = "GTF File to create TxDb"
-    ),
-    make_option(c("-c", "--cores"),
-        type = "integer",
-        default = 1,
-        metavar = "integer",
-        help = "Number of cores."
-    )
+parser <- ArgumentParser(description = "Run groHMM on some bam files")
+
+parser$add_argument(
+    "-i",
+    "--bam_files",
+    type = "character",
+    nargs = "+",
+    metavar = "path",
+    help = "GRO SEQ data in bam files.",
+    required = TRUE
+)
+parser$add_argument(
+    "-t",
+    "--tuning_file",
+    type = "character",
+    default = NULL,
+    metavar = "path",
+    help = "File with tuning parameters and error rates."
+)
+parser$add_argument(
+    "-o",
+    "--outdir",
+    type = "character",
+    default = "./",
+    metavar = "path",
+    help = "Output directory."
+)
+parser$add_argument(
+    "-l",
+    "--ltprobb",
+    type = "integer",
+    default = -200,
+    metavar = "integer",
+    help = "Log-transformed transition probability of switching from transcribed state to non-transcribed state"
+)
+parser$add_argument(
+    "-u",
+    "--uts",
+    type = "integer",
+    default = 5,
+    metavar = "integer",
+    help = "Variance of the emission probability for reads in the non-transcribed state, respectively."
+)
+parser$add_argument(
+    "-p",
+    "--outprefix",
+    type = "character",
+    default = "grohmm",
+    metavar = "string",
+    help = "Output prefix."
+)
+parser$add_argument(
+    "-g",
+    "--gtf",
+    type = "character",
+    default = NULL,
+    metavar = "string",
+    help = "GTF File to create TxDb",
+    required = TRUE
+)
+parser$add_argument(
+    "-c",
+    "--cores",
+    type = "integer",
+    default = 1,
+    metavar = "integer",
+    help = "Number of cores."
 )
 
-opt_parser <- OptionParser(option_list = option_list)
-opt <- parse_args(opt_parser)
+args <- parser$parse_args()
 
-
-if (is.null(opt$bam_file)) {
-    print_help(opt_parser)
-    stop("Please provide a bam file", call. = FALSE)
-}
-
-# Read in bam file.
-if (file.exists(opt$outdir) == FALSE) {
-    dir.create(opt$outdir, recursive = TRUE)
-}
-setwd(opt$outdir)
+setwd(args$outdir)
 
 # Begin use of groHMM
 # TODO CURRENTLY ONLY TAKES ONE FILE
 ## readsfile <- as(
 ##     GenomicAlignments::readGAlignments(
-##         file = opt$bam_file,
+##         file = args$bam_file,
 ##         use.names = TRUE
 ##     )
 ## )
 # TODO CHANGE BASED ON PAIRED OR SINGLE END
-galigned <- readGAlignments(BamFile(opt$bam_file, asMates = TRUE))
+# FIXME
+galigned <- readGAlignments(BamFile(args$bam_file, asMates = TRUE))
 reads_file <- GRanges(galigned)
 
 # Call annotations > DEFAULT VALUES ASSIGNED
-if (is.null(opt$tuning_file)) {
+if (is.null(args$tuning_file)) {
     hmm_result <- detectTranscripts(
         reads_file,
-        LtProbB = opt$ltprobb,
-        UTS = opt$uts,
+        LtProbB = args$ltprobb,
+        UTS = args$uts,
         threshold = 1
     ) # Uses either inputted or default values
 } else {
-    tune <- read.csv(opt$tuning_file)
+    tune <- read.csv(args$tuning_file)
     # Minimum error
     uts <- tune[which.min(tune$total), "UTS"]
     lt_probb <- tune[which.min(tune$total), "LtProbB"]
@@ -113,26 +120,26 @@ if (is.null(opt$tuning_file)) {
 tx_hmm <- hmm_result$transcripts
 write.table(
     tx_hmm,
-    file = paste(opt$outprefix,
+    file = paste(args$outprefix,
         ".transcripts.txt",
         sep = ""
     )
 )
 
 # Input transcript annotations
-kg_db <- makeTxDbFromGFF(opt$gtf)
+kg_db <- makeTxDbFromGFF(args$gtf)
 kg_tx <- transcripts(kg_db, columns = c("gene_id", "tx_id", "tx_name"))
 # Collapse annotations in preparation for overlap
 kg_consensus <- makeConsensusAnnotations(
     kg_tx,
     keytype = "gene_id",
-    mc.cores = opt$cores
+    mc.cores = args$cores
 )
 
 # Evaluate HMM Annotations
 e <- evaluateHMMInAnnotations(tx_hmm, kg_consensus)
 # Save as txt file
-capture.output(e$eval, file = paste0(opt$outprefix, ".eval.txt"))
+capture.output(e$eval, file = paste0(args$outprefix, ".eval.txt"))
 
 # TUNING IN A DIFFERENT SCRIPT
 
@@ -152,13 +159,13 @@ b_plus <- breakTranscriptsOnGenes(tx_hmm, kg_consensus, strand = "+")
 b_minus <- breakTranscriptsOnGenes(tx_hmm, kg_consensus, strand = "-")
 tx_broken <- c(b_plus, b_minus)
 tx_final <- combineTranscripts(tx_broken, kg_consensus)
-td_final <- getTxDensity(tx_final, con_expressed, mc.cores = opt$cores)
-export(tx_final, con = paste(opt$outprefix, ".final.transcripts.bed", sep = ""))
-capture.output(td_final, file = paste0(opt$outprefix, ".tdFinal.txt"))
+td_final <- getTxDensity(tx_final, con_expressed, mc.cores = args$cores)
+export(tx_final, con = paste(args$outprefix, ".final.transcripts.bed", sep = ""))
+capture.output(td_final, file = paste0(args$outprefix, ".tdFinal.txt"))
 # Output plot
-jpeg(file = paste0(opt$outprefix, ".tdplot_mqc.jpg"))
+jpeg(file = paste0(args$outprefix, ".tdplot_mqc.jpg"))
 # 2. Create the plot
-td_final <- getTxDensity(tx_final, con_expressed, mc.cores = opt$cores)
+td_final <- getTxDensity(tx_final, con_expressed, mc.cores = args$cores)
 
 # 3. Close the file
 dev.off()
