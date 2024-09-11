5cf681dd4018aa12065afb61dd1cef6573b7fc7e
Edmund Miller <git@edmundmiller.dev>
2024-09-11 13:40:16 -0500

    style: Clean up groovy formatting

--- tests/lib/UTILS.groovy
+++ tests/lib/UTILS.groovy
@@ -1,6 +1,5 @@
-// Function to remove Nextflow version from pipeline_software_mqc_versions.yml
-
 class UTILS {
+    // Function to remove Nextflow version from pipeline_software_mqc_versions.yml
     public static Object removeNextflowVersion(pipeline_software_mqc_versions) {
         def softwareVersions = path(pipeline_software_mqc_versions).yaml
         if (softwareVersions.containsKey("Workflow")) {
@@ -9,37 +8,36 @@ class UTILS {
         return softwareVersions
     }
 
-// Recursively list all files in a directory and its sub-directories, matching a given suffix
-// TODO: use regex pattern instead of suffix?
-public static getAllFilesFromDir(dir, suffix) {
-    def output = []
-    new File(dir).eachFileRecurse() {
-        if (it.name.toString().endsWith(suffix)) {
-            output.add(it)
+    // Recursively list all files in a directory and its sub-directories, matching a given suffix
+    // TODO: use regex pattern instead of suffix?
+    public static getAllFilesFromDir(dir, suffix) {
+        def output = []
+        new File(dir).eachFileRecurse() {
+            if (it.name.toString().endsWith(suffix)) {
+                output.add(it)
+            }
         }
+        return output.sort()
     }
-    return output.sort()
-}
-// Recursively list all files names in a directory and its sub-directories, matching a given suffix, return file names
-public static getAllFileNamesFromDir(dir, suffix) {
-    def output = []
-    new File(dir).eachFileRecurse() {
-        if (it.name.toString().endsWith(suffix)) {
-            output.add(it.toString().split("/")[-1])
+    // Recursively list all files names in a directory and its sub-directories, matching a given suffix, return file names
+    public static getAllFileNamesFromDir(dir, suffix) {
+        def output = []
+        new File(dir).eachFileRecurse() {
+            if (it.name.toString().endsWith(suffix)) {
+                output.add(it.toString().split("/")[-1])
+            }
         }
+        return output.sort()
     }
-    return output.sort()
-}
 
-// Recursively list all files names in a directory and its sub-directories, matching a given suffix, return if check if given string is in file
-public static checkAllFilesNamesFromDirForString(dir, suffix, string) {
-    def output = []
-    new File(dir).eachFileRecurse() {
-        if (it.name.toString().endsWith(suffix)) {
-            output.add(it.text.contains(string))
+    // Recursively list all files names in a directory and its sub-directories, matching a given suffix, return if check if given string is in file
+    public static checkAllFilesNamesFromDirForString(dir, suffix, string) {
+        def output = []
+        new File(dir).eachFileRecurse() {
+            if (it.name.toString().endsWith(suffix)) {
+                output.add(it.text.contains(string))
+            }
         }
+        return output.sort()
     }
-    return output.sort()
-}
-
 }
